a721d79f554764e831aa01a77ce48aebb7b451b8
Daniel Sanches <danielsanches6301@gmail.com>
2022-01-20 20:32:47 -0300

    Definindo movimentos possíveis da torre

--- xadrez-console_c-sharp/Xadrez/Pecas/Torre.cs
+++ xadrez-console_c-sharp/Xadrez/Pecas/Torre.cs
@@ -11,7 +11,65 @@ namespace Xadrez
 
         public override bool[,] MovimentosPossiveis()
         {
-            throw new System.NotImplementedException();
+            bool[,] matriz = new bool[Tabuleiro.Linhas, Tabuleiro.Colunas];
+            Posicao pos = new Posicao(0, 0);
+
+            return matriz;
+
+            //acima
+            pos.DefinirValores(pos.Linha - 1, pos.Coluna);
+            while (Tabuleiro.PosicaoValida(pos) && PodeMover(pos))
+            {
+                matriz[pos.Linha, pos.Coluna] = true;
+                if(Tabuleiro.RetornarPeca(pos) != null && Tabuleiro.RetornarPeca(pos).Cor != Cor)
+                {
+                    break;
+                }
+                pos.Linha -= 1;
+            }
+
+            //abaixo
+            pos.DefinirValores(pos.Linha + 1, pos.Coluna);
+            while (Tabuleiro.PosicaoValida(pos) && PodeMover(pos))
+            {
+                matriz[pos.Linha, pos.Coluna] = true;
+                if (Tabuleiro.RetornarPeca(pos) != null && Tabuleiro.RetornarPeca(pos).Cor != Cor)
+                {
+                    break;
+                }
+                pos.Linha += 1;
+            }
+
+            //direita
+            pos.DefinirValores(pos.Linha, pos.Coluna + 1);
+            while (Tabuleiro.PosicaoValida(pos) && PodeMover(pos))
+            {
+                matriz[pos.Linha, pos.Coluna] = true;
+                if (Tabuleiro.RetornarPeca(pos) != null && Tabuleiro.RetornarPeca(pos).Cor != Cor)
+                {
+                    break;
+                }
+                pos.Coluna += 1;
+            }
+
+            //esquerda
+            pos.DefinirValores(pos.Linha, pos.Coluna - 1);
+            while (Tabuleiro.PosicaoValida(pos) && PodeMover(pos))
+            {
+                matriz[pos.Linha, pos.Coluna] = true;
+                if (Tabuleiro.RetornarPeca(pos) != null && Tabuleiro.RetornarPeca(pos).Cor != Cor)
+                {
+                    break;
+                }
+                pos.Coluna -= 1;
+            }
+        }
+
+        private bool PodeMover(Posicao pos)
+        {
+            Peca p = Tabuleiro.RetornarPeca(pos);
+
+            return p == null || p.Cor != Cor;
         }
 
         public override string ToString()
